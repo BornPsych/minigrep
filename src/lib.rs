7da611740049ce216f4f340dd9ce22808ff0aac9
Yogesh Shahi <yogeshshahi120@gmail.com>
2025-03-01 16:36:31 +0530

    completed mini grep

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,18 @@ pub struct Config{
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
         
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         
@@ -42,24 +46,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 // We tell Rust that the data returned by the search function will live as long as the data passed into the search function in the contents argument.
 
 pub fn search<'a>(query:&str, content:&'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
-    for line in content.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    return results;
+   content.
+   lines().
+   filter(|line| line.contains(query)).
+   collect()
 }
 
 pub fn search_case_insensitive<'a>(query:&str, content:&'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
-    let query = query.to_lowercase();
-    for line in content.lines(){
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-        }
-    }
-    results
+    content.
+    lines().
+    filter(|line| line.to_lowercase().
+    contains(&query.to_lowercase())).collect()
+
 }
 
 #[cfg(test)]
